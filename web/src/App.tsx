34a4b5cd9c7e37a84e24f083ef9216944e126775
devilzcore <aldosilva1@live.com>
2020-08-04 19:24:20 -0300

    Add landing page with react native

---
+++ web/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Landing from './pages/Landing/index'
+
+import './assets/styles/global.css'
+
+function App() {
+  return (
+    <Landing />
+  );
+}
+
+export default App;
