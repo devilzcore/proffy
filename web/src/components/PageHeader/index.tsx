ef177458ac81e1e36f3d105019a43040ebbc439a
devilzcore <aldosilva1@live.com>
2020-08-07 22:50:29 -0300

    Add page give classes

--- web/src/components/PageHeader/index.tsx
+++ web/src/components/PageHeader/index.tsx
@@ -8,6 +8,7 @@ import './styles.css';
 
 interface pageHeaderProps {
     title: string;
+    description?: string;
 }
 
 const PageHeader: React.FC<pageHeaderProps> = (props) => {
@@ -22,6 +23,7 @@ const PageHeader: React.FC<pageHeaderProps> = (props) => {
 
             <div className="header-content">
                 <strong>{props.title}</strong>
+                { props.description && <p>{props.description}</p> }
 
                 {props.children}
             </div>
